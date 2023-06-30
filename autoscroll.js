b1568481bf644bee6ec97354027c2aa3aad02065
AF-Bismarck <133364655+AF-Bismarck@users.noreply.github.com>
2023-06-30 12:52:27 -0700

    Add files via upload

---
+++ autoscroll.js
@@ -0,0 +1,22 @@
+(function(host){ 
+        var interval = ms, increment = 1, pairs = window.location.search.substring(1).split("&");
+
+        for(var i = 0; i < pairs.length; i++){
+          var pair = pairs[i].split("="),
+              attribute = pair[0],
+              value = parseInt(pair[1]);
+
+          switch(attribute){
+            case "interval": interval = value;
+            case "increment": increment = value;
+          }         
+        }
+
+        return { 
+          start: function(){ 
+            host.scrollBy(0, increment); 
+            host.setTimeout(arguments.callee, interval) 
+          } 
+        }; 
+      }(this)).start();
+completion(true);
